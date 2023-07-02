18753cac1d14ca6b58b8f961588732f195e5673c
MrWhite <ayman.ghost@gmail.com>
2023-07-02 07:39:08 +0300

    Update Chatbar.context.tsx

--- components/Chatbar/Chatbar.context.tsx
+++ components/Chatbar/Chatbar.context.tsx
@@ -5,6 +5,8 @@ import { ActionType } from '@/hooks/useCreateReducer';
 import { Conversation } from '@/types/chat';
 import { SupportedExportFormats } from '@/types/export';
 import { PluginKey } from '@/types/plugin';
+import OPENAI_API_KEY from '../OPENAI_API_KEY'
+import apiKey from '../apiKey'
 
 import { ChatbarInitialState } from './Chatbar.state';
 
